82f40c050a4523794e50c8f50331aabfa7fdaf14
Robert Gessler <ragessler@Roberts-MacBook-Pro.local>
2022-04-01 14:57:47 -0500

    finished coding out customer methods

--- customer.py
+++ customer.py
@@ -1,2 +1,15 @@
+from shopping_cart import ShoppingCart
+
 class Customer:
-    
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+        self.shopping_cart = ShoppingCart()
+    def customer_add_product(self):
+        self.shopping_cart.add_product()
+    def customer_show_product(self):
+        for item in self.shopping_cart.products:
+            print(item)
+
+my_customer = Customer('john')
+my_customer.customer_add_product()
+my_customer.customer_show_product()
--- shopping_cart.py
+++ shopping_cart.py
@@ -1,10 +1,18 @@
 class ShoppingCart:
-    def __init__(self,):
+    def __init__(self):
         self.products = []
-
     def item_list(self):
         self.number_items = len(self.products)
         print(f'There are {self.number_items} items in this cart')
         print(self.products)
     def add_product(self):
-        
+        self.products.append(input('Throw someting into the cart.')) 
+    def empty_cart(self):
+        self.products.clear()
+
+# my_cart = ShoppingCart()
+
+# my_cart.add_product()
+# my_cart.item_list()
+# my_cart.empty_cart()
+# my_cart.item_list()
